1cbd96dc775635fefd013f49d4f145f25ecbcd46
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-01-30 16:56:27 +1100

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
     
+// };
+
+// export default nextConfig;
+import path from 'path';
+
+export default {
+  webpack(config) {
+    const __dirname = path.dirname(new URL(import.meta.url).pathname); // 替代 __dirname
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),  // 设置 '@' 为 'src' 目录的别名
+    };
+    return config;
+  },
 };
 
-export default nextConfig;
+
